4339a453dfe5e004515ab3e61a12333e81119946
zohair22 <Zohairosama321@gmail.com>
2022-03-20 13:45:41 +0200

    existing file

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Admin
+class Admin extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
 
